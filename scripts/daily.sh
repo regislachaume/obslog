4cd7ac333feeab54af482463cfc20db5a64577fa
loqueelvientoajuarez <regis.lachaume@gmail.com>
2022-05-23 13:12:58 -0400

    automatic update of 2022-05-23

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -1,6 +1,41 @@
 #! /bin/sh
 
-cd /home/regis/git/autolog/scripts
-./periodlog.py > logs/`date -I`.log
+MAINDIR=/home/regis/git/autolog
+SCRIPTDIR=${MAINDIR}/scripts
+WWWDIR=/data/www/twoptwo.com
+DATE=`date -I`
+TIME=`date -Iseconds`
+LOG=${SCRIPTDIR}/logs/${DATE}.log
+
+
+touch $LOG
+echo "" >> $LOG
+echo "daily.sh at $TIME" >> $LOG
+echo "-------------------------------------" >> $LOG
+echo "" >> $LOG
+
+cd ${MAINDIR}
+echo "github pull in ${MAINDIR}" >> $LOG
+git pull >> $LOG
+
+cd ${WWWDIR}
+echo "github pull in ${WWWDIR}" >> $LOG
+git pull >> $LOG 
+
+cd ${SCRIPTDIR}
+./periodlog.py >> $LOG
+
+cd ${MAINDIR}
+echo "github push in ${MAINDIR}" > $LOG
+git add * >> $LOG 
+git commit -m"automatic update of ${DATE}" >> $LOG
+git push >> $LOG
+
 cd /data/www/twoptwo.com
-./sync.sh
+echo  "sync ${WWWDIR} to live website http://www.astro.puc.cl/2.2m" >> $LOG
+./sync.sh >> $LOG
+
+echo "github push in ${WWWDIR}" > $LOG
+git add * >> $LOG
+git commit -m"automatic update of ${DATE}" >> $LOG
+git push >> $LOG

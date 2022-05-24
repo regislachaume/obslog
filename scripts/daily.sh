a1ee74dbf724f21fd5caad7f5257b4e8874541b1
loqueelvientoajuarez <regis.lachaume@gmail.com>
2022-05-24 13:21:02 -0400

    automatic update of 2022-05-24

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -6,40 +6,50 @@ WWWDIR=/data/www/twoptwo.com
 DATE=`date -I`
 TIME=`date -Iseconds`
 LOG=${SCRIPTDIR}/logs/${DATE}.log
+ERR=${SCRIPTDIR}/logs/${DATE}.err
 
 
 touch $LOG
-echo "" >> $LOG
-echo "daily.sh at $TIME" >> $LOG
-echo "-------------------------------------" >> $LOG
-echo "" >> $LOG
+touch $ERR
+cat<<EOF  >> $LOG >>& $ERR
+
+daily.sh at $TIME
+-------------------------------------
+
+EOF 
 
 cd ${MAINDIR}
-echo "github pull in ${MAINDIR}" >> $LOG
-git pull >> $LOG 2>> $LOG
+echo "github pull in ${MAINDIR}" >> $LOG >>& $ERR
+git pull >> $LOG 2>> $ERR
+echo ""  >> $LOG >>& $ERR
 
 cd ${WWWDIR}
-echo "github pull in ${WWWDIR}" >> $LOG
-git pull >> $LOG 2>> $LOG
+echo "github pull in ${WWWDIR}" >> $LOG >>& $ERR
+git pull >> $LOG 2>> $ERR
+echo ""  >> $LOG >>& $ERR
 
 cd ${SCRIPTDIR}
-./periodlog.py >> $LOG 2>> $LOG
+echo "executing periodlog.py in ${SCRIPTDIR}" >> $LOG >>& $ERR
+./periodlog.py >> $LOG 2>> $ERR
+echo ""  >> $LOG >>& $ERR
 
 cd ${MAINDIR}
-echo "git add * in ${MAINDIR}" >> $LOG
-git add * >> $LOG 2>> $LOG
-echo "git commit" >> $LOG
-git commit -m"automatic update of ${DATE}" >> $LOG 2>> $LOG
-echo "git push" >> $LOG
-git push >> $LOG 2>> $LOG
+echo "git add * in ${MAINDIR}" >> $LOG >>& $ERR
+git add * >> $LOG 2>> $ERR
+echo "git commit" >> $LOG  >>& $ERR
+git commit -m"automatic update of ${DATE}" >> $LOG 2>> $ERR
+echo "git push" >> $LOG  >>& $ERR
+git push >> $LOG 2>> $ERR
+echo ""  >> $LOG >>& $ERR
 
-cd /data/www/twoptwo.com
+cd ${WWWDIR}
 echo  "sync ${WWWDIR} to live website http://www.astro.puc.cl/2.2m" >> $LOG
 ./sync.sh >> $LOG 2>> $LOG
+echo ""  >> $LOG >>& $ERR
 
-echo "git add * in ${WWWDIR}" >> $LOG
+echo "git add * in ${WWWDIR}" >> $LOG >>& $ERR
 git add * >> $LOG 2>> $LOG
-echo "git commit"
-git commit -m"automatic update of ${DATE}" >> $LOG 2>> $LOG
-git "push"
-git push >> $LOG 2>> $LOG
+echo "git commit" >> $LOG >>& $ERR
+git commit -m"automatic update of ${DATE}" >> $LOG 2>> $ERR
+git push >> $LOG 2>> $ERR
+echo ""  >> $LOG >>& $ERR

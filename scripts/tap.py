b9609c9697b3da3d2613863a56f1333aa8f833ea
loqueelvientoajuarez <regis.lachaume@gmail.com>
2022-05-25 15:00:23 -0400

    automatic update of 2022-05-25

--- scripts/tap.py
+++ scripts/tap.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3
+#! /usr/bin/env python3.9
 
 import sys
 sys.path.append('..')
@@ -7,8 +7,7 @@ from autolog.eso.tapquery import NightQuery
 
 import numpy as np
 
-night = '2021-01-01'
+night = sys.argv[1]
 telescope = 'ESO-2.2m'
 query = NightQuery(telescope=telescope)
 log1 = query.fetch(night=night, use_tap_cache=False)
-log2 = query.fetch(night=night, use_tap_cache=True)
